0aa78174651330906a12d7e69e7525590bad2efe
Sondra Sohn <48785364+0hyodorie0@users.noreply.github.com>
2021-03-10 10:03:28 +0900

    Add files via upload

---
+++ header.php
@@ -0,0 +1,53 @@
+<div class="bg_wrap">
+    <nav id="top">
+        <!-- 로고, 메뉴, 네비 -->
+        <div class="container">
+            <div class="basenav">
+                <ul class="basemenu">
+                    <li class="baseitem">
+                        <a href="#">로그인</a>
+                    </li>
+                    <li class="baseitem">
+                        <a href="#">회원가입</a>
+                    </li>
+                    <li class="baseitem">
+                        <a href="#">사이트맵</a>
+                    </li>
+                    <li class="baseitem">
+                        <a href="#">English</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="main_nav">
+                <div class="logo">
+                    <h1 class="logo_text"><a href="index1.html" title="Ciel HS">HS 조회시스템</a></h1>
+                </div>
+                <form action="">
+                    <div class="search_option">
+                        <!-- <select name="" id="">
+                                    <option value="통합검색">통합검색</option>
+                                    <option value="관세율표">관세율표</option>
+                                    <option value="분류사례">분류사례</option>
+                                    <option value="이미지">이미지</option>
+                                    <option value="주·해설">주·해설</option>
+                                    <option value="법령검색">법령검색</option>
+                                    <option value="판례·예규">판례·예규</option>
+                                    <option value="HS비교">HS비교</option>
+                                </select>-->
+                        <div class="search">
+                            <input type="text" class="search_text" placeholder="검색어를 입력하세요">
+                            <div class="search_btn" onclick="location.href='index2.html'"><i class="fas fa-search"></i></div>
+                        </div>
+                    </div>
+                </form>
+                <ul class="menu">
+                    <li class="menuitem"><a href="index3.html">관세율표</a></li>
+                    <li class="menuitem"><a href="index7.html">세계 HS</a></li>
+                    <li class="menuitem"><a href="#">분류사례</a></li>
+                    <li class="menuitem"><a href="#">관련법령</a></li>
+                    <li class="menuitem"><a href="#">무역통계</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
